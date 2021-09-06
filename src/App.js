696ee43513b040cbd7952d008864bb331c19b5b1
david.manurung <david.manurung@au.yusen-logistics.com>
2021-09-06 21:57:38 +1000

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Lets do something harder and more advance!!!
         </p>
         <a
           className="App-link"
